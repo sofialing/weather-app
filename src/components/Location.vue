f88df9f28c4c6f11835092ea59f5b95e5b026236
Sofia Ling <sofialing@live.se>
2020-01-06 11:20:54 +0100

    Temporarily hidden search icon

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,39 +1,39 @@
 <template>
-	<section>
-		<img :src="searchIcon" class="search-icon" />
-		<p>{{ location }}</p>
-	</section>
+  <section>
+    <!-- <img :src="searchIcon" class="search-icon" /> -->
+    <p>{{ location }}</p>
+  </section>
 </template>
 
 <script>
 export default {
-	name: 'Location',
-	props: {
-		location: {
-			type: String
-		},
-		time: {
-			type: String
-		}
-	},
-	computed: {
-		searchIcon() {
-			return require(`../assets/icons/search-${this.time}.svg`);
-		}
-	}
+  name: "Location",
+  props: {
+    location: {
+      type: String
+    },
+    time: {
+      type: String
+    }
+  },
+  computed: {
+    searchIcon() {
+      return require(`../assets/icons/search-${this.time}.svg`);
+    }
+  }
 };
 </script>
 
 <style scoped>
 .search-icon {
-	width: 1.5rem;
-	position: absolute;
-	top: 1rem;
-	right: 1rem;
+  width: 1.5rem;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
 }
 p {
-	font-size: 1.25rem;
-	font-weight: 700;
-	letter-spacing: 0.05em;
+  font-size: 1.25rem;
+  font-weight: 700;
+  letter-spacing: 0.05em;
 }
 </style>
