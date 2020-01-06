92ce634445d19e7872d45044ea2861da080466fb
Sofia Ling <sofialing@live.se>
2020-01-06 11:19:36 +0100

    Adjusted css grid

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,48 +1,49 @@
 <template>
-	<section>
-		<div v-for="item in forecast" :key="item.day">
-			<p class="day">{{ item.day }}</p>
-			<img :src="item.icon" class="weather-icon" />
-			<p class="temp">{{ item.temp }}&deg;</p>
-		</div>
-	</section>
+  <section>
+    <div v-for="item in forecast" :key="item.day">
+      <p class="day">{{ item.day }}</p>
+      <img :src="item.icon" class="weather-icon" />
+      <p class="temp">{{ item.temp }}&deg;</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	name: 'Forecast',
-	props: {
-		forecast: {
-			type: Array
-		}
-	}
+  name: "Forecast",
+  props: {
+    forecast: {
+      type: Array
+    }
+  }
 };
 </script>
 
 <style scoped>
 section {
-	display: grid;
-	grid-template-columns: repeat(5, auto);
-	gap: 2rem;
+  display: grid;
+  grid-auto-columns: auto;
+  grid-auto-flow: column;
+  gap: 2rem;
 }
 
 div {
-	min-width: 40px;
-	text-align: center;
+  min-width: 40px;
+  text-align: center;
 }
 
 .day {
-	text-transform: uppercase;
-	margin-bottom: 0.5rem;
+  text-transform: uppercase;
+  margin-bottom: 0.5rem;
 }
 
 .temp {
-	font-size: 1.25rem;
-	font-weight: 700;
+  font-size: 1.25rem;
+  font-weight: 700;
 }
 
 .weather-icon {
-	height: 24px;
-	margin-bottom: 0.5rem;
+  height: 24px;
+  margin-bottom: 0.5rem;
 }
 </style>
