4876d88687820f4c5794d475c241af421e0a6aac
Sofia Ling <sofialing@live.se>
2020-01-20 17:21:07 +0100

    Fixed issue with  API endpoint

--- src/services/appServices.js
+++ src/services/appServices.js
@@ -1,5 +1,5 @@
 const API = {
-	current: 'http://api.openweathermap.org/data/2.5/weather?units=metric',
+	current: 'https://api.openweathermap.org/data/2.5/weather?units=metric',
 	forecast: 'https://api.openweathermap.org/data/2.5/forecast?units=metric',
 	key: '&APPID=5c6c3b43fd40fea14df38a848bebf667'
 };
