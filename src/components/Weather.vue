67e809da3fd0796440f45cae1eb318aa3996a22b
Sofia Ling <sofialing@live.se>
2020-02-11 11:35:49 +0100

    Implemented pwa

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -22,7 +22,7 @@ export default {
 			required: true
 		},
 		timeOfDay: {
-			type: Boolean,
+			type: String,
 			required: true
 		}
 	},
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueGeolocation from 'vue-browser-geolocation';
+import './registerServiceWorker'
 Vue.use(VueGeolocation);
 
 Vue.config.productionTip = false;
